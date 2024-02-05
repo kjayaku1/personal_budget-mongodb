21c3a6a7314a265f867f7311692ad42811da6a48
kalai joys <kjayaku1@uncc.edu>
2024-02-05 00:10:12 -0500

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use('/', express.static('public'));
+const budget = {
+    myBudget: [
+    {
+        title: 'Travel',
+        budget: 20
+    },
+    {
+        title: 'Gas',
+        budget: 15
+    },
+    {
+        title: 'Mobile Bill',
+        budget: 15
+    },
+    {
+        title: 'Rent',
+        budget: 375
+    },
+    {
+        title: 'Groceries',
+        budget: 80
+    },
+    {
+        title: 'Internet',
+        budget: 20
+    },
+    {
+        title: 'Other',
+        budget: 50
+    },
+]};
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+})
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}')
+});
